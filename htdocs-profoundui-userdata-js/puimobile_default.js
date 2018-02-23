832305111311f60177b5048cf7136b17043fc16b
Jeff Berman <jeff@gonif.org>
2018-02-23 11:47:31 -0800

    Added UI and much more functionality for sample app and web service

--- htdocs-profoundui-userdata-js/puimobile_default.js
+++ htdocs-profoundui-userdata-js/puimobile_default.js
@@ -3,9 +3,7 @@
  */
 
 // Place all end-user code in `pui.mobile`
-pui.mobile = (function (ns) {
-  var done = null;
-
+pui.mobile = (function () {
   /**
   * Perform sync
   * If any local order data exists, send it to the server, then get back an updated
@@ -16,11 +14,11 @@ pui.mobile = (function (ns) {
   * @param {string} connection.port The port number for this connection.
   * @param {boolean} connection.ssl Use https?
   * @param {string} connection.parm The parameter to use with this connection.
-  * @param {string} connection.baseURL A convenience property containing the base URL for this connection, formatted using the above properties (e.g., "https://yourServer:8080")
-  * @param {string} connection.syncURL A convenience property containing a formatted `sync` URL (e.g., "https://yourServer:8080/sync?parameter=abc")
+  * @param {string} connection.baseURL A convenience property containing the base URL for this connection, formatted using the above properties (e.g., "https://yourServer:8080").
+  * @param {string} connection.syncURL A convenience property containing a formatted `sync` URL (e.g., "https://yourServer:8080/sync?parameter=abc").
   * @param {function} callback Callback function that must be called when the sync process has completed.
   */
-  ns.sync = function (connection, callback) {
+  function sync(connection, callback) {
     var config = {
       headers: { "Content-Type": "application/json" },
       url: connection.syncURL,
@@ -31,27 +29,25 @@ pui.mobile = (function (ns) {
     // Retrieve order info from local storage
     var orders = getObject("orders");
 
-    if (orders || true) {
-      // config.postData = JSON.stringify({ orders: orders });
-      config.postData = JSON.stringify({ orders: [{ orid: 1, ordtlid: 1, orprid: 2579, orqty: 1, orprice: 56.73 }] });
+    if (orders) {
+      config.postData = JSON.stringify({ orders: orders });
     }
 
     // Send sync request to server
-    done = callback;
     ajaxJSON(config);
-  }
 
-  /**
-  * AJAX sync request handler
-  */
-  function syncHandler(response) {
-    // The response will be an updated Products file, so store it to the
-    // device and remove the orders that have already been sent.
-    if (response.products) {
-      storeObject("products", response.products);
-      removeObject("orders");
+    /**
+    * AJAX sync request handler
+    */
+    function syncHandler(response) {
+      // The response will be an updated Products file, so store it to the
+      // device and remove the orders that have already been sent.
+      if (response.products) {
+        storeObject("products", response.products);
+        removeObject("orders");
+      }
+      callback();
     }
-    done();
   }
 
   /**
@@ -77,14 +73,92 @@ pui.mobile = (function (ns) {
     window.localStorage.removeItem(key);
   }
 
-  return ns;
-})(pui.mobile || {});
+  /**
+   * Run the offline app
+   * @param {object} displayData JSON data that describes the offline app's display content.
+   * @param {function} callback Callback function that must be called when the run process has completed.
+   */
+  function run(displayData, callback) {
+    // Initialize data
+    // Retrieve product data from local storage
+    var products = getObject("products") || [];
+    var savedOrders = getObject("orders") || [];
+
+    savedOrders.qtyPurchasedForId = function (id) {
+      var qty = this.filter(function (e) {
+        return e.orprid == id;
+      });
+
+      if (qty.length === 0) {
+        return 0;
+      } else {
+        return qty[0].orqty;
+      }
+    };
+
+    // Update products table with saved order counts
+    products.forEach(function (product) {
+        product.orqty = savedOrders.qtyPurchasedForId(product.prid);
+    })
+
+    // Create config object that will be passed to pui.show()
+    var config = {
+      meta: displayData,
+      format: "Orders",
+      data: { ProdGrid: products },
+      handler: displayHandler
+    }
+
+    // Display screen and process user input
+    pui.show(config);
+
+    return;
 
 
-pui.mobile = (function (ns) {
-  ns.run = function () {
+    // Order screen handler -- called when user takes UI action
+    function displayHandler(response) {
+      console.log(response);
 
-  };
+      if (response["exit"] === "1") {
+        callback();
 
-  return ns;
-})(pui.mobile || {});
+        return;
+      }
+    }
+  }
+
+  /**
+   * Store shopping cart data when checking out
+   * @param {object} data Grid data from grid.getAllDataValues() -- provided by checkout button's Onclick event.
+   */
+  function checkout(data) {
+    var isOrder = function (product) {
+      return product.orqty > 0;
+    };
+    var buildOrders = function (product, index) {
+      var order = {};
+
+      order.ordtlid = index+1;
+      order.orprid = product.prid;
+      order.orqty = product.orqty;
+      order.orprice = product.prprice;
+      return order;
+    };
+    var orders = data
+      .filter(isOrder)
+      .map(buildOrders);
+
+    removeObject("orders");
+
+    if (orders.length > 0) {
+      storeObject("orders", orders);
+    }
+  }
+
+  // Return public methods
+  return {
+    sync: sync,
+    run: run,
+    checkout: checkout
+  }
+})();
